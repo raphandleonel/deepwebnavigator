04a216007633f46d922b4cc25d1b94349a2c8e33
joel-emoh7 <joel.e@mblhightech.com>
2025-01-13 02:34:55 +0200

    feat: added search on mobile and added animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import {
   AcademicCapIcon,
   HomeIcon,
